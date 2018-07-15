1f6750a8b8a45e58f6d2213fadaa11dd3876dc4d
justforwebcoding <iromjkeecorp@yandex.ru>
2018-07-15 12:33:20 +0300

    basic functions added

---
+++ notes-functions.js
@@ -0,0 +1,115 @@
+// Read existing notes from localStorage
+const getSavedNotes = function () {
+    const notesJSON = localStorage.getItem('notes')
+
+    if (notesJSON !== null) {
+        return JSON.parse(notesJSON)
+    } else {
+        return []
+    }
+}
+
+// Save the notes to localStorage
+const saveNotes = function (notes) {
+    localStorage.setItem('notes', JSON.stringify(notes))
+}
+
+
+//Remove a note from the list
+const removeNote = function(id) {
+    const noteIndex = notes.findIndex(function(note) {
+        return note.id === id
+    })
+
+    if(noteIndex > -1) {
+        notes.splice(noteIndex, 1)
+    }
+
+} 
+// Generate the DOM structure for a note
+const generateNoteDOM = function (note) {
+    const noteEl = document.createElement('div')
+    const textEl = document.createElement('span')
+    const button = document.createElement('button')
+    const link = document.createElement('a')
+    link.setAttribute('href', `/edit.html#${note.id}`)
+    
+
+    if (note.title.length > 0) {
+        textEl.textContent = note.title
+    } else {
+        textEl.textContent = 'Unnamed note'
+    }
+    
+    link.appendChild(textEl)
+    noteEl.appendChild(link)
+
+    button.textContent ='x'
+    noteEl.appendChild(button)
+    button.addEventListener('click', function(e) {
+        removeNote(note.id)
+        saveNotes(notes)
+        renderNotes(notes, filters)
+    })
+    
+
+    return noteEl
+}
+
+//Sort your notes by selection
+const sortNotes = function(notes, sortBy) {
+    if(sortBy === 'byEdited') {
+        return notes.sort(function (a, b) {
+            if(a.updatedAt > b.updatedAt) {
+                return -1
+            } else if(a.updatedAt < b.updatedAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if(sortBy === 'byCreated'){
+        return notes.sort(function (a, b) {
+            if(a.createdAt > b.createdAt) {
+                return -1
+            } else if(a.createdAt < b.createdAt) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    } else if(sortBy === 'alphabetical'){
+        return notes.sort(function (a, b) {
+            if (a.title.toLowerCase() < b.title.toLowerCase()) {
+                return -1
+            } else if (a.title.toLowerCase() > b.title.toLowerCase()) {
+                return 1
+            } else {
+                return 0
+            }
+        })
+    }
+}
+
+
+// Render application notes
+const renderNotes = function (notes, filters) {
+    notes = sortNotes(notes, filters.sortBy)
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#notes').innerHTML = ''
+
+    filteredNotes.forEach(function (note) {
+        const noteEl = generateNoteDOM(note)
+        document.querySelector('#notes').appendChild(noteEl)
+    })
+}
+
+
+//Generate the last edited message
+const generateLastEdited = function (timestamp) {
+    return `Last edited ${moment(timestamp).fromNow()}`
+}
+
